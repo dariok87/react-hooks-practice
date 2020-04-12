9e050d4f88532dc53a983051a359fdb8b5665411
Dario <dkemenovic@gmail.com>
2020-04-12 13:23:07 +0200

    Add state and divs

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 import React, { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about",
+      isCompleted: false,
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "Build app",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list"></div>
+    </div>
+  );
+}
+
+export default App;
