ac980ce4e8148f3c79cc6e1af04c50299ae86316
Dario <dkemenovic@gmail.com>
2020-04-20 22:51:04 +0200

    Create delete button

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function Todo({ todo, index, completeTodo }) {
       {todo.text}
       <div>
         <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => deleteTodo(index)}>X</button>
       </div>
     </div>
   );
